85da383134fecab52565e367b26c657485379f1b
Micint0sh <1155092088@link.cuhk.edu.hk>
2017-09-24 12:42:18 +0800

    Added done-to-todo function and delete button in done objects. Refracted jquery.js .

--- resources/js/jquery.js
+++ resources/js/jquery.js
@@ -1,25 +1,30 @@
 $(document).ready(function (){
 	var timer;
-	var timerRMTrashCan;
 	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 	console.log(width);
 	if(width > 1000)
 	{
 		//When hover over 1 second on li
-		toggleDeleteButtonDesktop(1000);
+		toggleDeleteButtonDesktop(1000,"#todo");
+		toggleDeleteButtonDesktop(1000,"#done");
 		// When clicked the trashCan image
-		$("#todo").on("click","li > button.trashCan",removeTrashCan);
+		$("#todo").on("click","li > button.trashCan",removeParentObject);
+		$("#done").on("click","li > button.trashCan",removeParentObject);
 		//When clicked the tick image
 		$("#todo").on("click","li > button.todoTick",toDone);
+		$("#done").on("click","li > button.doneTick",toToDo);
 	}
 	else
 	{
 		//When detected swipe right action
-		toggleDeleteButtonMobile(10);
+		toggleDeleteButtonMobile(10,"#todo");
+		toggleDeleteButtonMobile(10,"#done");
 		// When tapped the trashCan image
-		$("#todo").on("touchstart","li > button.trashCan",removeTrashCan);
+		$("#todo").on("touchend","li > button.trashCan",removeParentObject);
+		$("#done").on("touchend","li > button.trashCan",removeParentObject);
 		// When tapped the tick image
 		$("#todo").on("touchstart","li > button.todoTick",toDone);
+		$("#done").on("touchstart","li > button.doneTick",toToDo);
 	}
 
 });
@@ -32,8 +37,9 @@ $(document).ready(function (){
 //Functions Used
 var pixelsMovedRight = 0;
 var prevPosX = 0;
-//Adds a TrashCan button into element
-function addTrashCan(element){
+
+//Adds a trashCan HTML into element
+function addTrashCanToHTML(element){
 	var trashItem = document.createElement("button");
 	trashItem.classList.add("trashCan");
 	trashItem.innerHTML = trashSVG;
@@ -41,8 +47,23 @@ function addTrashCan(element){
 	return trashItem;
 }
 
-//Removes the parent item
-function removeTrashCan() {
+//Adds a trashCan button into the object
+function addTrashCan(object)
+{
+	//Adding an delete button if there is not one
+	if( $(object).children("button.trashCan").length == 0 )
+	{
+		var trashItem = addTrashCanToHTML(object);
+		trashItem = jQuery(trashItem);
+		//Animation
+		$(object).animate({"text-indent":"45px"},200,function (){
+			trashItem.animate({"opacity":"1"},300);
+		});
+	}
+}
+
+//Removes the parent object
+function removeParentObject() {
 	//Animation
 	var jqList = $(this).parent();
 	jqList.slideUp(200,function(){
@@ -50,78 +71,52 @@ function removeTrashCan() {
 	});
 }
 
-function toggleDeleteButtonDesktop(delay) {
-	$("#todo").on("mouseenter","li",function (){
-		var theObject = this;
+//Remove a trashCan object from element
+function removeTrashCan(object) {
+	if($(object).children("button.trashCan").length != 0 ) // If trashCan exists
+	{
+		var trashItem = $(object).children("button.trashCan");
+		trashItem.animate({"opacity":"0"},100,function(){
+			trashItem.remove();
+		});
+		//Setting back the text-indent
+		$(object).animate({"text-indent":"0px"},300);
+	}
+}
+
+function toggleDeleteButtonDesktop(delay,selector) {
+	$(selector).on("mouseenter","li",function (){
 		//For desktop operations
+		var theObject = this;
 		timer=setTimeout(function (){
-				var jqTheObject= jQuery(theObject);
-				//Adding an delete button if there is not one
-				if( $(this).children("button.trashCan").length == 0 )
-				{
-					var trashItem = addTrashCan(theObject);
-					trashItem = jQuery(trashItem);
-					//Animation
-					jqTheObject.animate({"text-indent":"45px"},200,function (){
-						trashItem.animate({"opacity":"1"},300);
-					});
-				}
-			},delay);
+				addTrashCan(theObject);
+		},delay);
 		}).on("mouseleave","li",function(){
 			clearTimeout(timer);
-			if( $(this).children("button.trashCan").length != 0 )
-			{
-				var trashItem = $(this).children("button.trashCan");
-				trashItem.animate({"opacity":"0"},100,function(){
-					trashItem.remove();
-				});
-				//Setting back the text-indent
-				$(this).animate({"text-indent":"0px"},300);
-			}
+			removeTrashCan(this);
 		});
 }
 
-function toggleDeleteButtonMobile(distance) {
-	$("#todo").on("touchmove","li",function(event){
+function toggleDeleteButtonMobile(distance,selector) {
+	$(selector).on("touchmove","li",function(event){
 		//For mobile operations :
-		console.log("Move detected\n");
 		var touch = event.targetTouches[0];
 		if( prevPosX ==0 )
 		{
 			prevPosX = touch.pageX;
-			console.log("prevPosX initiated!\n");
 		}
 		else
 		{
 			pixelsMovedRight += touch.pageX - prevPosX;
-			console.log("pixelsMovedRight: "+pixelsMovedRight);
 			prevPosX = touch.pageX;
 		}
 		if(pixelsMovedRight >= distance)
 		{
-			console.log("Distance exceeded!");
-			if($(this).children("button.trashCan").length == 0 )
-			{
-				console.log("trashCan shown.");
-				var trashItem = addTrashCan(this);
-				trashItem = jQuery(trashItem);
-				//Animation
-				$(this).animate({"text-indent":"45px"},200,function (){
-					trashItem.animate({"opacity":"1"},300);
-				});
-			}
+			addTrashCan(this);
 		}
 		if(-pixelsMovedRight >= distance)
 		{
-			if($(this).children("button.trashCan").length != 0 )
-			{
-				var trashItem = $(this).children("button.trashCan");
-				trashItem.animate({"opacity":"0"},200,function(){
-					trashItem.remove();
-				});
-				//Setting back the text-indent
-				$(this).animate({"text-indent":"0px"},200);
-			}
+			removeTrashCan(this);
 		}
 	}).on("touchend","li",function(){
 		prevPosX = 0;
@@ -130,12 +125,27 @@ function toggleDeleteButtonMobile(distance) {
 }
 
 function toDone() {
+	if($(this).children("button.trashCan").length != 0)
+	{
+		toggleDeleteButtonDesktop();
+	}
 	var thisObject = this;
 	var jqList = $(this).parent();
 	jqList.slideUp(200,function(){
 		$(thisObject).removeClass("todoTick").addClass("doneTick");
 		var li = jqList.html();
 		$("#done").append("<li>"+li+"</li>");
+		jqList.remove();
+	});
+}
+
+function toToDo() {
+	var thisObject = this;
+	var jqList = $(this).parent();
+	jqList.slideUp(200,function(){
+		$(thisObject).removeClass("doneTick").addClass("todoTick");
+		var li = jqList.html();
+		$("#todo").append("<li>"+li+"</li>");
 		jqList.remove();
 	});
 }
